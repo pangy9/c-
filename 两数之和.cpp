07790b1e4303e5d0ac5f8d6e7d219f37e18e5c3b
庞韵 <115408337+pangy9@users.noreply.github.com>
2022-10-21 23:10:19 +0800

    Update 两数之和.cpp

--- 两数之和.cpp
+++ 两数之和.cpp
@@ -1,33 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
- # include<stdio.h>
- # include<stdlib.h>
-int* twoSum(int* nums, int numsSize, int target){
-    int jump=0,i,j;
-    for(i=0;i<numsSize;i++){
-        for(j=i+1;j<numsSize;j++){
-            if((nums[i]+nums[j])==target){
-                printf("[%d,%d]",i,j);
-                jump=1;
-                break;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i,j,sum;
+    int* ret;
+    for(i = 0;i < numsSize;i++){
+        for(j = i + 1;j < numsSize;j++){
+            sum = nums[i] + nums[j];
+            if(sum == target){
+                ret = malloc(sizeof(int)*2);
+                ret[0]=i;
+                ret[1]=j; 
+               *returnSize = 2;
+                return ret;
             }
-            if(jump == 1) break;
         }
     }
-}
-int main(){
-    int numsSize,target,i=0;
-    int* nums;
-    int* twoSum(int*,int,int);
-    printf("ÇëÊäÈëÊý×énums = {...}, target = ...\n");
-	scanf("nums = {");
-    do{
-    	scanf("%d",&nums[i]);
-    	i ++;
-	}while(i!='}');
-    scanf(", target = %d",&target);
-    numsSize = sizeof(nums)/sizeof(nums)[0];
-    twoSum(nums,numsSize,target);
-    return 0;
+    *returnSize = 0;
+    free (ret);
+    return NULL;
 }
